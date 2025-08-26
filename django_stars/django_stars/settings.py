8409030ca475680d711c3b7da3a7baade5ad3f70
miniusercoder <admin@miniuser.ru>
2025-08-26 21:23:27 +0300

    DB engine choosing

--- django_stars/django_stars/settings.py
+++ django_stars/django_stars/settings.py
@@ -79,8 +79,22 @@ WSGI_APPLICATION = "django_stars.django_stars.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": (
+            BASE_DIR / "db.sqlite3"
+            if os.getenv("DB_ENGINE", "django.db.backends.sqlite3")
+            == "django.db.backends.sqlite3"
+            else os.getenv("DB_NAME")
+        ),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": (
+            {"charset": "utf8mb4"}
+            if os.getenv("DB_ENGINE") == "django.db.backends.mysql"
+            else {}
+        ),
     }
 }
 
