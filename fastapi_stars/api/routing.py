79094211653b05dce469d6eefaabac30781c4d80
miniusercoder <admin@miniuser.ru>
2025-08-29 18:17:38 +0300

    Fix order router

--- fastapi_stars/api/routing.py
+++ fastapi_stars/api/routing.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from .v1 import auth, users, info
+from .v1 import auth, users, info, order
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/v1/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/v1/users", tags=["users"])
 api_router.include_router(info.router, prefix="/v1/info", tags=["info"])
-api_router.include_router(info.router, prefix="/v1/orders", tags=["orders"])
+api_router.include_router(order.router, prefix="/v1/orders", tags=["orders"])
