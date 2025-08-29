6858a1989f93e331de4e9cd41358a78a238d2804
miniusercoder <admin@miniuser.ru>
2025-08-29 19:31:59 +0300

    Delete debug

--- django_stars/django_stars/settings.py
+++ django_stars/django_stars/settings.py
@@ -9,8 +9,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-logger.debug(BASE_DIR)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
