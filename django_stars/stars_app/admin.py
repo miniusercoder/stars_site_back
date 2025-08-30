4c7b3ffe20db4bf4a51ba788a405a230fcde85cc
miniusercoder <admin@miniuser.ru>
2025-08-30 18:29:51 +0300

    Django admin updated

--- django_stars/stars_app/admin.py
+++ django_stars/stars_app/admin.py
@@ -1,9 +1,43 @@
-# Register your models here.
 from django.contrib import admin
 
-from django_stars.stars_app.models import Price
+from django_stars.stars_app.models import Price, User, Order, PaymentSystem, Payment
 
 
 @admin.register(Price)
 class PriceAdmin(admin.ModelAdmin):
     list_display = ("type", "price", "white_price")
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "wallet_address", "created_at", "referrer")
+    search_fields = (
+        "id",
+        "wallet_address",
+    )
+    list_filter = ("created_at",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "type", "status", "created_at", "updated_at")
+    search_fields = (
+        "id",
+        "user__wallet_address",
+        "recipient_username",
+    )
+    list_filter = ("type", "status", "created_at")
+
+
+@admin.register(PaymentSystem)
+class PaymentSystemAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_active")
+    search_fields = ("name",)
+    list_filter = ("is_active",)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "amount", "status", "created_at")
+    search_fields = ("order__id",)
+    list_filter = ("status", "created_at")
