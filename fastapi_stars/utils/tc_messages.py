30a3246bc0c938d0a2fe80514477ff5e26afa90d
miniusercoder <admin@miniuser.ru>
2025-09-01 02:32:39 +0300

    Non-bounceable on TON top-up

--- fastapi_stars/utils/tc_messages.py
+++ fastapi_stars/utils/tc_messages.py
@@ -53,7 +53,7 @@ def build_tonconnect_message(
             "validUntil": int(time.time()) + 300,
             "messages": [
                 {
-                    "address": user_jetton_wallet_address.to_str(),
+                    "address": user_jetton_wallet_address.to_str(is_bounceable=False),
                     "amount": str(to_nano(0.05)),
                     "payload": jetton_transfer_message,
                 }
