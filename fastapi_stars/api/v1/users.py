174525529e3347ece9f53696d0ebb3c074619fd2
miniusercoder <admin@miniuser.ru>
2025-09-04 16:11:35 +0300

    User stats for profile

--- fastapi_stars/api/v1/users.py
+++ fastapi_stars/api/v1/users.py
@@ -1,11 +1,12 @@
 from typing import Annotated, Optional
 
-from django.db.models import Q
+from django.db.models import Q, Sum
 from fastapi import APIRouter, Depends
 from fastapi.params import Query
 
-from django_stars.stars_app.models import Order, Payment
+from django_stars.stars_app.models import Order, Payment, User
 from fastapi_stars.api.deps import Principal, user_principal
+from fastapi_stars.schemas.info import PriceWithCurrency, PricesWithCurrency
 from fastapi_stars.schemas.users import (
     UserOut,
     SuccessResponse,
@@ -14,15 +15,79 @@ from fastapi_stars.schemas.users import (
     OrderModel,
     PaymentsResponse,
     PaymentModel,
+    UserStatistic,
+    StatsForOrderType,
 )
+from integrations.Currencies import USDT
 
 router = APIRouter()
 
 
+def get_my_orders_stats(user: User, order_type: Order.Type) -> tuple[int, float]:
+    orders = Order.objects.filter(
+        is_refund=False,
+        user=user,
+        type=order_type,
+        status__in=(Order.Status.COMPLETED, Order.Status.BLOCKCHAIN_WAITING),
+    )
+    orders_amount = orders.aggregate(Sum("amount"))["amount__sum"]
+    orders_price = orders.aggregate(Sum("price"))["price__sum"]
+    return orders_amount or 0, orders_price or 0
+
+
 @router.get("/me", response_model=UserOut)
 def me(principal: Principal = Depends(user_principal)):
+    stars_stats = get_my_orders_stats(principal["user"], Order.Type.STARS)
+    premium_stats = get_my_orders_stats(principal["user"], Order.Type.PREMIUM)
+    ton_stats = get_my_orders_stats(principal["user"], Order.Type.TON)
+    total_deposit = (
+        Payment.objects.filter(
+            order__user=principal["user"], status=Payment.Status.CONFIRMED
+        ).aggregate(Sum("sum"))["sum__sum"]
+        or 0
+    )
+    user_stats = UserStatistic(
+        stars=StatsForOrderType(
+            amount=stars_stats[0],
+            price=PricesWithCurrency(
+                price_usd=PriceWithCurrency(price=stars_stats[1], currency="usd"),
+                price_rub=PriceWithCurrency(
+                    price=USDT.usd_to_rub(stars_stats[1]), currency="rub"
+                ),
+            ),
+        ),
+        premium=StatsForOrderType(
+            amount=premium_stats[0],
+            price=PricesWithCurrency(
+                price_usd=PriceWithCurrency(price=premium_stats[1], currency="usd"),
+                price_rub=PriceWithCurrency(
+                    price=USDT.usd_to_rub(premium_stats[1]), currency="rub"
+                ),
+            ),
+        ),
+        ton=StatsForOrderType(
+            amount=ton_stats[0],
+            price=PricesWithCurrency(
+                price_usd=PriceWithCurrency(price=ton_stats[1], currency="usd"),
+                price_rub=PriceWithCurrency(
+                    price=USDT.usd_to_rub(ton_stats[1]), currency="rub"
+                ),
+            ),
+        ),
+        deposit=PricesWithCurrency(
+            price_usd=PriceWithCurrency(price=total_deposit, currency="usd"),
+            price_rub=PriceWithCurrency(
+                price=USDT.usd_to_rub(total_deposit), currency="rub"
+            ),
+        ),
+    )
     user = principal["user"]
-    return UserOut.model_validate(user)
+    return UserOut(
+        id=user.id,
+        wallet_address=user.wallet_address,
+        ref_alias=user.ref_alias,
+        stats=user_stats,
+    )
 
 
 @router.post("/ref_alias", response_model=SuccessResponse)
--- fastapi_stars/schemas/users.py
+++ fastapi_stars/schemas/users.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel, ConfigDict, Field, UUID4, field_validator
 
 from django_stars.stars_app.models import Order, Payment
-from fastapi_stars.schemas.info import PaymentMethodModel
+from fastapi_stars.schemas.info import (
+    PaymentMethodModel,
+    PricesWithCurrency,
+)
+
+
+class StatsForOrderType(BaseModel):
+    amount: int
+    price: PricesWithCurrency
+
+
+class UserStatistic(BaseModel):
+    stars: StatsForOrderType
+    premium: StatsForOrderType
+    ton: StatsForOrderType
+    deposit: PricesWithCurrency
 
 
 class UserOut(BaseModel):
@@ -9,6 +24,7 @@ class UserOut(BaseModel):
     id: int
     wallet_address: str
     ref_alias: str | None
+    stats: UserStatistic
 
 
 class SuccessResponse(BaseModel):
@@ -44,6 +60,7 @@ class PaymentModel(BaseModel):
     sum: float
     status: Payment.Status
     created_at: int
+
     @field_validator("created_at", mode="before")
     @classmethod
     def create_date_validator(cls, value):
